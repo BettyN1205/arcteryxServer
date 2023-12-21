2f5f8d5fe6bc3102463be8241fe209367d792ff1
BettyNiu <bettychaoranniu@gmail.com>
2023-12-21 10:38:09 -0800

    update dataset

--- routes/clothing-routes.js
+++ routes/clothing-routes.js
@@ -4,9 +4,9 @@ const router = require("express").Router();
 router.get("/:id", async (req, res) => {
   try {
     const itemId = req.params.id;
-    const data = await knex("clothing_size")
-    .select('clothing_size.*')
-    .where('clothing_size.product_id', '=', itemId);
+    const data = await knex("man_shirt")
+    .select('man_shirt.*')
+    .where('man_shirt.Product_ID', '=', itemId);
     res.status(200).json(data);
   } catch (err) {
     res.status(400).send(`Error retrieving Users: ${err}`);
